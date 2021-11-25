6e059159aaa820bc0cd69e1b4623695964e908c5
Kevin Mason <krmason2020@gmail.com>
2021-11-25 04:06:51 +0000

    completed SQL Injection assignment

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,17 +1,26 @@
-pool
-  .query(
-    `
+const { Pool } = require("pg");
+
+const values = [process.argv[2] || "JUL02"];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`
-  )
-  .then((res) => {
-    res.rows.forEach((row) => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    });
+`;
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((row) => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   });
+});
